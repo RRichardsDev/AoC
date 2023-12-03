aa1e05dd51bc6029d4c51b3e5181018ac9a143fc
Rhodri Richards <rhodri.devlopment@gmail.com>
2023-12-03 18:57:32 +0000

    day 3 COMPLETED

--- 2023/day-03/part-1.go
+++ 2023/day-03/part-1.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 )
 
+// Define structs for line numbers, symbol indices, and part numbers.
 type LineNum struct {
 	value int
 }
@@ -22,10 +23,12 @@ type PartNumber struct {
 	nextIndex     int
 }
 
+// Global maps to store symbols' indices and part numbers.
 var linesSymbolsIdxs = make(map[LineNum][]SymbolIdx)
 var partNumbers = make(map[LineNum][]PartNumber)
 var sum int
 
+// Part1 reads a file and calculates a sum based on certain conditions.
 func Part1() int {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -40,41 +43,48 @@ func Part1() int {
 		line := scanner.Text()
 		lineNum++
 
+		// Regular expressions to identify symbols and numbers.
 		isSymbol := regexp.MustCompile(`[^\w\s.]`)
 		isNumber := regexp.MustCompile(`\d+`)
 
 		lineNumTyped := LineNum{lineNum}
+		println("---" + strconv.Itoa(lineNum) + "---")
 		symbolIdxPairs := isSymbol.FindAllStringIndex(line, -1)
 		var symbolIdxs []SymbolIdx
 		for _, symbolIdxsPair := range symbolIdxPairs {
 			symbolIdxs = append(symbolIdxs, SymbolIdx{symbolIdxsPair[0]})
 		}
+		// linesSymbolsIdxs[139] = [23,34,51,65,76,79,84,91,94,104,110,133]
 		linesSymbolsIdxs[lineNumTyped] = append(linesSymbolsIdxs[lineNumTyped], symbolIdxs...)
 
 		numPairs := isNumber.FindAllString(line, -1)
 		var numPairsInt []int
 		for _, numPair := range numPairs {
+			println(numPair)
 			asInt, _ := strconv.Atoi(numPair)
 			numPairsInt = append(numPairsInt, asInt)
 		}
 
-		// indexes of numbers
+		// Getting indexes of numbers.
 		numIdxPairs := isNumber.FindAllStringIndex(line, -1)
 
-		// zip to PartNumber - numbers with extended to previous and next indexes
+		// Pair numbers with their neighboring indices.
 		for index, numIndePair := range numIdxPairs {
 			previousNeighbour := 0
 			if numIndePair[0] > 0 {
 				previousNeighbour = numIndePair[0] - 1
 			}
+			// partNumbers[139] = {value: 23, previousIndex: 22, nextIndex: 24}
 			partNumbers[lineNumTyped] = append(partNumbers[lineNumTyped], PartNumber{numPairsInt[index], previousNeighbour, numIndePair[1]})
 		}
 	}
 
+	// Process each part number and calculate the sum.
 	for lineNumber, partNumbers := range partNumbers {
 		for _, partNumber := range partNumbers {
 			shouldBeCounted := false
 
+			// Retrieve symbol indices from adjacent lines.
 			previousLineSymbolIndexes := linesSymbolsIdxs[LineNum{lineNumber.value - 1}]
 			thisLineSymbolIndexes := linesSymbolsIdxs[LineNum{lineNumber.value}]
 			nextLineSymbolIndexes := linesSymbolsIdxs[LineNum{lineNumber.value + 1}]
@@ -82,6 +92,7 @@ func Part1() int {
 			partNumberMinIndex := partNumber.previousIndex
 			partNumberMaxIndex := partNumber.nextIndex
 
+			// Check if the number is adjacent to any symbol on the same or neighboring lines.
 			for _, previousLineSymbolIndex := range previousLineSymbolIndexes {
 				if previousLineSymbolIndex.index >= partNumberMinIndex && previousLineSymbolIndex.index <= partNumberMaxIndex {
 					shouldBeCounted = true
@@ -104,6 +115,7 @@ func Part1() int {
 				}
 			}
 
+			// Add the number's value to sum if it meets the condition.
 			if shouldBeCounted {
 				sum += partNumber.value
 			}
--- 2023/day-03/part-2.go
+++ 2023/day-03/part-2.go
@@ -1,5 +1,162 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"io/ioutil"
+	"strconv"
+	"unicode"
+)
+
+type Coord struct {
+	row int
+	col int
+}
+
+type Part struct {
+	number  int
+	engines []Coord
+}
+
+func getCoordsToCheck(rowIndex, from, to int, matrix [][]rune) []Coord {
+	maxRow, maxCol := len(matrix)-1, len(matrix[0])-1
+	var initialList []Coord
+	// above and botton row
+	for col := from - 1; col <= to+1; col++ {
+		initialList = append(
+			initialList,
+			Coord{row: rowIndex - 1, col: col},
+			Coord{row: rowIndex + 1, col: col},
+		)
+	}
+	initialList = append(initialList, Coord{row: rowIndex, col: from - 1}, Coord{row: rowIndex, col: to + 1})
+	var cleanList []Coord
+	for _, coord := range initialList {
+		row, col := coord.row, coord.col
+		if row >= 0 && row <= maxRow && col >= 0 && col <= maxCol {
+			cleanList = append(cleanList, coord)
+		}
+	}
+	return cleanList
+}
+func parseInput() [][]rune {
+	// lines := common.ReadFile("day3/input_small")
+	lines := ReadFile("input.txt")
+	var matrix [][]rune
+	for _, line := range lines {
+		var row []rune
+		for _, char := range line {
+			row = append(row, char)
+		}
+		matrix = append(matrix, row)
+	}
+	return matrix
+}
+
+func getEnginesAround(rowIndex, from, to int, matrix [][]rune) []Coord {
+	var engines []Coord
+	coordToCheck := getCoordsToCheck(rowIndex, from, to, matrix)
+	for _, coord := range coordToCheck {
+		char := matrix[coord.row][coord.col]
+		if char == '*' {
+			engines = append(engines, coord)
+		}
+	}
+	return engines
+}
+func ErrPanic(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ReadFile(path string) []string {
+	content, err := ioutil.ReadFile(path)
+	ErrPanic(err)
+	scanner := bufio.NewScanner(bytes.NewReader(content))
+	var lines []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+	return lines
+}
+
+func getEngineParts(parts []Part, engine Coord) []Part {
+	var engineParts []Part
+	for _, part := range parts {
+		for _, e := range part.engines {
+			if e.row == engine.row && e.col == engine.col {
+				engineParts = append(engineParts, part)
+			}
+		}
+	}
+	return engineParts
+}
+
+func calculateGearsRatio(parts [][]Part, engines [][]Coord, matrix [][]rune) int {
+	var sum int
+	for i, row := range engines {
+		for _, engine := range row {
+			var engineParts []Part
+			if i > 0 {
+				engineParts = append(engineParts, getEngineParts(parts[i-1], engine)...)
+			}
+			engineParts = append(engineParts, getEngineParts(parts[i], engine)...)
+			if i < (len(engines) - 1) {
+				engineParts = append(engineParts, getEngineParts(parts[i+1], engine)...)
+			}
+			if len(engineParts) == 2 {
+				sum += (engineParts[0].number * engineParts[1].number)
+			}
+		}
+	}
+
+	return sum
+}
+func getPartsAndEngines(matrix [][]rune) ([][]Part, [][]Coord) {
+	parts := make([][]Part, len(matrix))
+	engines := make([][]Coord, len(matrix))
+	var start, end int
+	var number string
+	var parsingNumber bool
+	for i, row := range matrix {
+		start, end = 0, 0
+		number, parsingNumber = "", false
+		for j, char := range row {
+			if char == '*' {
+				engines[i] = append(engines[i], Coord{row: i, col: j})
+			}
+			if unicode.IsDigit(char) {
+				number += string(char)
+			}
+			if unicode.IsDigit(char) && !parsingNumber {
+				parsingNumber = true
+				start = j
+			}
+			if !unicode.IsDigit(char) && parsingNumber {
+				end = j - 1
+				// check
+				enginesAround := getEnginesAround(i, start, end, matrix)
+				n, err := strconv.Atoi(number)
+				ErrPanic(err)
+				part := Part{number: n, engines: enginesAround}
+				parts[i] = append(parts[i], part)
+				number, start, end, parsingNumber = "", 0, 0, false
+			}
+		}
+		if parsingNumber {
+			enginesAround := getEnginesAround(i, start, len(row)-1, matrix)
+			n, err := strconv.Atoi(number)
+			ErrPanic(err)
+			part := Part{number: n, engines: enginesAround}
+			parts[i] = append(parts[i], part)
+		}
+	}
+	return parts, engines
+}
 func Part2() int {
-	return 0
+	matrix := parseInput()
+	parts, engines := getPartsAndEngines(matrix)
+	return calculateGearsRatio(parts, engines, matrix)
 }
