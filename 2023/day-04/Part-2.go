614ba30a267b798f9fa06808ab12f4c86fd192eb
Rhodri Richards <rhodri.devlopment@gmail.com>
2023-12-04 20:58:00 +0000

    day 4 COMPLETED

--- 2023/day-04/Part-2.go
+++ 2023/day-04/Part-2.go
@@ -1,6 +1,102 @@
 package main
 
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
 func Part2() int {
-	print("Part 2!")
-	return 0
+	lines := countLines("input.txt")
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var played []int
+
+	for i := 0; i < lines; i++ {
+		played = append(played, 0)
+	}
+
+	index := 0
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		played[index] += 1
+
+		game := strings.Split(scanner.Text(), ": ")[1]
+
+		winning := strings.Split(game, " | ")[0]
+
+		ours := strings.Split(game, " | ")[1]
+
+		winnum := numSlice(winning)
+		ournum := numSlice(ours)
+
+		intersection := intersection(winnum, ournum)
+
+		for w := range intersection {
+			played[index+w+1] += played[index]
+		}
+
+		index++
+	}
+	total := 0
+	for i := range played {
+		total += played[i]
+	}
+	return total
+}
+
+func countLines(file string) int {
+	lines := 0
+	content, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer content.Close()
+
+	scan := bufio.NewScanner(content)
+	for scan.Scan() {
+		lines++
+	}
+	return lines
+}
+
+func numSlice(nss string) []int {
+	numstrsl := strings.Fields(nss)
+
+	var numintsl []int
+
+	for _, n := range numstrsl {
+		num, err := strconv.Atoi(n)
+		if err != nil {
+			log.Fatal(err)
+		}
+		numintsl = append(numintsl, num)
+	}
+
+	return numintsl
+}
+func intersection(s1, s2 []int) []int {
+	m := make(map[int]bool)
+	var intersection []int
+
+	for _, item := range s1 {
+		m[item] = true
+	}
+
+	for _, item := range s2 {
+		if m[item] {
+			intersection = append(intersection, item)
+			// Optional: to avoid duplicates in the output
+			m[item] = false
+		}
+	}
+
+	return intersection
 }
--- 2023/day-04/main.go
+++ 2023/day-04/main.go
@@ -4,5 +4,5 @@ import "fmt"
 
 func main() {
 	fmt.Printf("Part 1: %d\n", Part1())
-	println(Part2())
+	fmt.Printf("Part 2: %d\n", Part2())
 }
